e75f0f818fade55509af195f428e15a35a0a1229
Shakib7784 <shakib7784@gmail.com>
2022-03-19 09:37:17 +0600

    fetch done

--- JS/Phone_Exploration.js
+++ JS/Phone_Exploration.js
@@ -1,1 +1,14 @@
-console.log("HEllo");
+
+// getting phone from input field then fetch phones
+const getphone = ()=>{
+    const getphones = document.getElementById("input");
+    const getphonesvalue = getphones.value;
+    // clear previous data
+    getphones.value="";
+    // fetch data
+    const url = ` https://openapi.programming-hero.com/api/phones?search=${getphonesvalue}`;
+    fetch(url)
+    .then(Response=>Response.json())
+    .then(data=>console.log(data));
+
+}
