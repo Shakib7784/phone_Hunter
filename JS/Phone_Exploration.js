476dcc8f4ac4860326fc600cffae34ca9f6e2842
Shakib7784 <shakib7784@gmail.com>
2022-03-19 09:58:27 +0600

    display done

--- JS/Phone_Exploration.js
+++ JS/Phone_Exploration.js
@@ -9,6 +9,31 @@ const getphone = ()=>{
     const url = ` https://openapi.programming-hero.com/api/phones?search=${getphonesvalue}`;
     fetch(url)
     .then(Response=>Response.json())
-    .then(data=>console.log(data));
+    .then(data=>displayphones(data.data));
+}
+
+//display phones
+const displayphones = phones=>{
+    const display = document.getElementById("display");
+    // clear prvious data
+    display.innerHTML="";
+    phones.forEach(phone=>{
+        const div = document.createElement('div');
+        div.classList.add("col");
+        div.innerHTML=`
+        <div class="col">
+              <div class="card">
+                <img src="${phone.image}" class="card-img-top" alt="...">
+                <div class="card-body">
+                  <h5 class="card-title">${phone.brand}</h5>
+                  <p class="card-text">${phone.phone_name}</p>
+                  <button  class="btn btn-outline-secondary" type="button" id="button-addon2">See Details</button>
+                </div>
+              </div>
+            </div>
+        `;
+        display.appendChild(div);
+    })
+
 
 }
